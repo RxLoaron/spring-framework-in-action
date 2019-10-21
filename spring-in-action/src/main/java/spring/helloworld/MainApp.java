19140e9dc7819625138f282ffe45ccb5723889ce
LuoYP <lyp3314@gmail.com>
2019-10-21 20:15:23 +0800

    add license

--- spring-in-action/src/main/java/spring/helloworld/MainApp.java
+++ spring-in-action/src/main/java/spring/helloworld/MainApp.java
@@ -1,7 +1,9 @@
 package spring.helloworld;
 
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
 /**
@@ -18,25 +20,25 @@ public class MainApp {
         //        IHelloWorld helloWorld = (IHelloWorld) context.getBean("helloworld");
         //        helloWorld.sayHello("Spring 4");
         //
-        
-        //        //XML based configuration
-        //        ApplicationContext xmlContext = new ClassPathXmlApplicationContext("classpath:spring-context.xml");
-        //        IHelloWorld helloWorld1 = (IHelloWorld) xmlContext.getBean("helloworld");
-        //        helloWorld1.sayHello("Spring 4,from xml...");
-        //        BeanFactory bf = new XmlBeanFactory(new ClassPathResource("spring-context.xml"));
+    
+        //XML based configuration
+        ApplicationContext xmlContext = new ClassPathXmlApplicationContext("classpath:spring-context.xml");
+        IHelloWorld helloWorld1 = (IHelloWorld) xmlContext.getBean("helloworld");
+        helloWorld1.sayHello("Spring 4,from xml...");
+        BeanFactory bf = new XmlBeanFactory(new ClassPathResource("spring-context.xml"));
         //        IHelloWorld helloWorld1 = (IHelloWorld) bf.getBean("helloworld");
         //        helloWorld1.sayHello("Spring 4,from BeanFactory...");
-        
+        //
         //        ClassPathResource resource = new ClassPathResource("spring-context.xml");
         //        XmlBeanFactory factory = new XmlBeanFactory(resource);
         //        IHelloWorld helloWorld = (IHelloWorld) factory.getBean("helloworld");
         //        helloWorld.sayHello("hello xmlbeanfactory...");
-        
-        ClassPathResource resource = new ClassPathResource("spring-context.xml");
-        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
-        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
-        reader.loadBeanDefinitions(resource);
-        IHelloWorld helloWorld = (IHelloWorld) factory.getBean("helloworld");
-        helloWorld.sayHello("programing");
+    
+        //        ClassPathResource resource = new ClassPathResource("spring-context.xml");
+        //        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+        //        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+        //        reader.loadBeanDefinitions(resource);
+        //        IHelloWorld helloWorld = (IHelloWorld) factory.getBean("helloworld");
+        //        helloWorld.sayHello("programing");
     }
 }
